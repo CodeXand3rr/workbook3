c87b65634eab3fa48577a91d565ada6a1a66e745
CodeXand3rr <jballesteross235@gmail.com>
2024-05-01 13:20:42 -0400

    Examples

---
+++ excercises/strings/parsing.js
@@ -0,0 +1,15 @@
+"use strict"
+ 
+ let name = "Brenda Kaye"
+ let positionOfSpace = name.indexOf(" ");  
+ console.log(positionOfSpace)
+ 
+ let firstName = name.substring  (0, positionOfSpace)
+ console.log(firstName);
+ 
+let lastName = name.substring(positionOfSpace +1);
+console.log(lastName)
+
+let message = `Your first name is ${firstName} and your last name is ${lastName}`
+console.log(message)
+
