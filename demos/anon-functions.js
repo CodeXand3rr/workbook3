9075f581aa05d0cf7f65258fb2c5434e0ab4e9aa
CodeXand3rr <jballesteross235@gmail.com>
2024-05-02 14:28:55 -0400

    car rental

---
+++ demos/anon-functions.js
@@ -0,0 +1,14 @@
+"use strict";
+
+// function greet() {
+//     console.log("hello")
+// }
+
+let greet = function () {
+  console.log(hello);
+};
+
+
+// let greet = ()=> console.log("hello");
+
+greet()
